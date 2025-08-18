d7b9263fc0ffd92a2d0c9af3d08240d4bb84ff1c
Julian Homez <julianhomesdev@icloud.com>
2025-08-18 00:00:44 -0500

    Added params in chatcard

--- src/Components/Chat/ChatCard.jsx
+++ src/Components/Chat/ChatCard.jsx
@@ -1,4 +1,4 @@
-export default function ChatCard() {
+export default function ChatCard(userPhoto, userName, lastMessage) {
 
     // This is the component for the chat cards with the main info of the user in the
     // sidebar chats
